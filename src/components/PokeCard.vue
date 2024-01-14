bf9568a57f68789bd0b3327e55d42def2e009aa9
facundo1996 <ffaacu1@gmail.com>
2024-01-14 11:51:28 -0300

    Add PokeCard

---
+++ src/components/PokeCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    PokeCard
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "PokeCard",
+}
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
-  <main>
-    
-  </main>
+  <div class="container">
+    <div class="pokemon-card-container">
+      <PokeCard />
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+  import PokeCard from '../components/PokeCard.vue'
+  export default {
+    name: "HomePage",
+    components: {
+      PokeCard
+    }
+  }
+</script>
